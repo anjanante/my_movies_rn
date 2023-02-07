050e74f11ffc43c4b9b1c84f219e95e1ea31dc7c
nanteOS <hrajaona@outsourcia-group.com>
2023-02-07 10:56:21 +0300

    Add tabNavigator

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Search from '../components/Search';
 import FilmDetail from '../components/FilmDetail';
 
@@ -15,4 +16,9 @@ const SearchStackNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(SearchStackNavigator);
+const MoviestabNavigator = createBottomTabNavigator({
+    Search:{
+        screen: SearchStackNavigator,
+    },
+})
+export default createAppContainer(MoviestabNavigator);
